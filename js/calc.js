98313f2866d213efdd1223f3acd953268ea504f8
Brikaa <brikaaomar@gmail.com>
2021-11-19 23:08:22 +0200

    Finalize calculator

--- js/calc.js
+++ js/calc.js
@@ -1,5 +1,97 @@
 (function () {
     document.getElementById('calculate').addEventListener('click', calculate);
+    document.getElementById('clear').addEventListener('click', clear_solution);
+    document.getElementById('vectors').addEventListener('change', clear_solution);
 
-    function calculate() {}
+    function get_number_array(input) {
+        const splitted = input.value.replaceAll(' + ', '+').replace(' - ', '-').split(' ');
+        const filtered = [];
+        for (let i = 0; i < splitted.length; ++i) {
+            splitted[i] = splitted[i]
+                .split('')
+                .filter((x) => x.match(/\d|i|\.|\+|-/g))
+                .join('');
+            try {
+                math.evaluate(splitted[i]);
+                filtered.push(splitted[i]);
+            } catch (e) {}
+        }
+        return filtered.filter((x) => x !== '');
+    }
+
+    function get_gram_vector(vectors, gram_vectors, current_vector) {
+        const base = math.evaluate(`[${vectors[current_vector]}]`);
+        if (current_vector === 0) {
+            return base;
+        }
+        let value = base;
+        for (let i = 1; i <= current_vector; ++i) {
+            const op = gram_vectors[current_vector - i];
+            value = math.evaluate(
+                `${value} - (${math.dot(op, base)} / ${math.dot(op, op)}) * ${op}`
+            );
+        }
+        return value;
+    }
+
+    function place_gram_vectors(gram_vectors) {
+        const solution_div = document.getElementById('solution-div');
+        for (let i = 0; i < gram_vectors.length; ++i) {
+            const sol = document.createElement('p');
+            const vectors_str = gram_vectors[i]
+                .map((x) => math.round(x, 4).toString())
+                ._data.join(', ');
+            sol.innerHTML = `(Y<sub>${i + 1}</sub>)<sup>T</sup> = [${vectors_str}]`;
+            solution_div.appendChild(sol);
+        }
+    }
+
+    function place_error_message() {
+        const solution_div = document.getElementById('solution-div');
+        const error = document.createElement('p');
+        error.innerHTML = 'Invalid vectors';
+        solution_div.appendChild(error);
+    }
+
+    function clear_solution() {
+        const solution_div = document.getElementById('solution-div');
+        solution_div.innerHTML = '';
+    }
+
+    function check_vectors_validity(vectors) {
+        if (
+            vectors.some((x) => x._data.some((o) => math.isNaN(o))) ||
+            vectors.some((v) => v._data.every((e) => math.round(e, 12) === 0))
+        ) {
+            console.log(vectors);
+            return false;
+        }
+        return true;
+    }
+
+    function calculate() {
+        clear_solution();
+        const vectors = [];
+        const inputs = document.getElementsByClassName('vector-input');
+        for (const input of inputs) {
+            vectors.push(get_number_array(input));
+        }
+        const size = vectors[0].length;
+        for (const vector of vectors) {
+            if (vector.length === 0) {
+                return;
+            }
+            if (vector.length !== size) {
+                return place_error_message();
+            }
+        }
+        const gram_vectors = [];
+        for (let i = 0; i < vectors.length; ++i) {
+            gram_vectors.push(get_gram_vector(vectors, gram_vectors, i));
+        }
+        if (!check_vectors_validity(gram_vectors)) {
+            return place_error_message();
+        }
+        place_gram_vectors(gram_vectors);
+    }
 })();
--- js/page.js
+++ js/page.js
@@ -2,6 +2,16 @@
     document.getElementById('vectors').addEventListener('change', set_vectors);
     document.getElementById('clear').addEventListener('click', clear_inputs);
 
+    (function() {
+        const select_box = document.getElementById('vectors');
+        for (let i = 1; i < 10; ++i) {
+            const option = document.createElement('option');
+            option.value = i + 1;
+            option.innerHTML = `${i + 1} vectors`;
+            select_box.appendChild(option);
+        }
+    })();
+
     function clear_vectors() {
         const vector_div = document.getElementById('vector-area');
         vector_div.innerHTML = '';
@@ -17,6 +27,7 @@
     function set_vectors(e) {
         clear_vectors();
         document.getElementById('control-buttons').style.visibility = 'visible';
+        document.getElementById('solution-div').style.visibility = 'visible';
         const vector_div = document.getElementById('vector-area');
         const no_vectors = e.target.value;
         for (let i = 0; i < no_vectors; ++i) {
